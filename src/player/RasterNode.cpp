c-base/libavg

commit 3d425cacce35560be1a45a1b40d6fe049558788c

Fixed FX Node memory leak.
svn path=/trunk/libavg/; revision=6575

src/player/RasterNode.cpp
@@ -125,6 +125,11 @@ void RasterNode::disconnect(bool bKill)
     }
     m_pFBO = FBOPtr();
     m_pImagingProjection = ImagingProjectionPtr();
+    if (bKill) {
+        m_pFXNode = FXNodePtr();
+    } else {
+        m_pFXNode->disconnect();
+    }
     AreaNode::disconnect(bKill);
 }
 

commit 2b0e06f0570347a851d2e958affa2c3cb754f5ec

Fixed crash caused by r6575.
svn path=/trunk/libavg/; revision=6579

src/player/RasterNode.cpp
@@ -128,7 +128,9 @@ void RasterNode::disconnect(bool bKill)
     if (bKill) {
         m_pFXNode = FXNodePtr();
     } else {
-        m_pFXNode->disconnect();
+        if (m_pFXNode) {
+            m_pFXNode->disconnect();
+        }
     }
     AreaNode::disconnect(bKill);
 }